f07cca789d7c7f8a062d8915263a7d33d67afcb0
Alan Rodriguez <alan.nicolas.rodriguez@gmail.com>
2020-05-03 11:32:29 -0300

    factory para las categorias y dejando test actuales

--- ComprandoEnCasaBackEnd/src/test/java/model/AppTest.java
+++ ComprandoEnCasaBackEnd/src/test/java/model/AppTest.java
@@ -1,46 +1,51 @@
 package model;
 
 
+import Modelo.App;
+import Modelo.Builder.AppBuilder;
+import Modelo.Cliente;
+import Modelo.Encargado;
+import Modelo.Factory.ClienteFactory;
+import Modelo.Factory.ProductoFactory;
 import com.ComprandoEnCasa.ComprandoEnCasaBackEnd.Entitys.Producto;
-import org.junit.Test;
 import junit.framework.TestCase;
-import Modelo.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppTest extends TestCase {
 
-    private Cliente gaston;
-    private Encargado oscar;
-    private Producto polenta;
-    private Producto pepsi;
-    private App app;
+    //private Cliente gaston;
+    //private Encargado oscar;
+    //private Producto polenta;
+    //private Producto pepsi;
+    //private App app;
+    private AppBuilder appBuilder = new AppBuilder();
 
-    public void setUp(){
-
-        oscar = new Encargado("oscar", "20-30656734-5", "oscar@gmail.com");
-        app = new App("alimentos/bebidas", "alberdi 333", "lunes a viernes de 10 a 20 hs", "efectivo, debito", 3, oscar);
-        gaston = new Cliente("gaston", "gaston@gmail.com", app, "alberdi 330");
-        polenta = new Producto("polenta", "presto pronta", 15, 80, "alguna imagen", "alimento");
-        pepsi = new Producto("gaseosa pepsi", "pepsi", 30, 120, "otra imagen", "bebida sin alcohol");
-
-
-    }
 
     @Test
     public void testCantidadClientes() {
-        assertTrue(app.cantidadDeClientes() == 0);
+        App app = appBuilder.build();
+        Cliente gaston = ClienteFactory.createWithNombre("Gaston");
+        assertEquals(app.cantidadDeClientes() , 0);
         app.agregarCliente(gaston);
-        assertTrue(app.cantidadDeClientes() == 1);
+        assertEquals(app.cantidadDeClientes() , 1);
     }
 
     @Test
     public void testDisponibilidadDeUnProducto(){
-        assertTrue(app.disponibilidadDeProducto(pepsi) == false);
+        //polenta = new Producto("polenta", "presto pronta", 15, 80, "alguna imagen", "alimento");
+        Producto polenta = ProductoFactory.createWithNombre("polenta");
+        //pepsi = new Producto("gaseosa pepsi", "pepsi", 30, 120, "otra imagen", "bebida sin alcohol");
+        Producto pepsi = ProductoFactory.createWithNombreAndMarca("Gaseosa","Pepsi");
+        App app = appBuilder.build();
+        assertFalse(app.disponibilidadDeProducto(pepsi));
         app.agregarProducto(polenta);
-        assertTrue(app.disponibilidadDeProducto(polenta) == true);
+        assertTrue(app.disponibilidadDeProducto(polenta));
         app.agregarProducto(pepsi);
-        assertTrue(app.disponibilidadDeProducto(pepsi) == true);
+        assertTrue(app.disponibilidadDeProducto(pepsi) );
         app.imprimirProductos();
     }
-
 
 }
