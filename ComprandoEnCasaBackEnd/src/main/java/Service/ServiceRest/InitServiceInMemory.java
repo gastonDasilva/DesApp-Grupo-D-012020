1e1a888cd7503d378e9160b9b4e0ecb39de25566
Alexander <alex.quinhonez@gmail.com>
2020-05-26 00:57:49 -0300

    agregado de funcionalidades en service y repository

---
+++ ComprandoEnCasaBackEnd/src/main/java/Service/ServiceRest/InitServiceInMemory.java
@@ -0,0 +1,40 @@
+package Service.ServiceRest;
+
+import javax.annotation.PostConstruct;
+
+import com.ComprandoEnCasa.ComprandoEnCasaBackEnd.Entitys.Producto;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class InitServiceInMemory {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @Value("${pring.datasource.driverClassName:NONE}")
+    private String className;
+
+    @Autowired
+    private ProductoService productoService;
+
+    @PostConstruct
+    public void initialize() {
+        if (className.equals("org.h2.Driver")) {
+            logger.warn("Init Data Using H2 DB");
+            fireInitialData();
+        }
+    }
+
+    private void fireInitialData(){
+        long lon = 1;
+        Producto producto = new Producto("fernet", "branca", 20, 290, "img_0001", "Bebida con alcohol", lon);
+        productoService.save(producto);
+    }
+
+}
--- ComprandoEnCasaBackEnd/src/main/java/com/ComprandoEnCasa/ComprandoEnCasaBackEnd/Repository/ProductoRepository.java
+++ ComprandoEnCasaBackEnd/src/main/java/com/ComprandoEnCasa/ComprandoEnCasaBackEnd/Repository/ProductoRepository.java
@@ -3,7 +3,19 @@ package com.ComprandoEnCasa.ComprandoEnCasaBackEnd.Repository;
 import com.ComprandoEnCasa.ComprandoEnCasaBackEnd.Entitys.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
+import java.util.Optional;
+
+
+@Configuration
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
+public interface ProductoRepository extends CrudRepository<Producto, Integer> {
+
+    Optional<Producto> findById(Integer id);
+
+    List<Producto> findAll();
+
 }
