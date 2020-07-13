6012865e9db2da19aa428ff53221e6e8dde657b7
Alexander <alex.quinhonez@gmail.com>
2020-07-12 21:25:17 -0300

    se agregaron nuevos productos al home de la aplicación

--- ComprandoEnCasaBackEnd/src/main/java/com/ComprandoEnCasa/ComprandoEnCasaBackEnd/Service/InitServiceInMemory.java
+++ ComprandoEnCasaBackEnd/src/main/java/com/ComprandoEnCasa/ComprandoEnCasaBackEnd/Service/InitServiceInMemory.java
@@ -49,11 +49,21 @@ public class InitServiceInMemory {
         producto = new Producto("Fideos Mostacholes", "Matarazzo", 12, 50, "https://i3-unileverar.a8e.net.br/gg/fideos-mostachol-matarazzo-500g_172827245_7790070318596.jpg", "Comida");
         productoService.save(producto);
 
-        producto = new Producto("Fernet Branca", "Branca", 20, 200, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDqKz5TenqnxQU6HoIy3uYgifJeoqutH5Xfyy0WtBxwrnlm0Po&usqp=CAU", "Comida");
+        producto = new Producto("Fernet Branca", "Branca", 20, 200, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDqKz5TenqnxQU6HoIy3uYgifJeoqutH5Xfyy0WtBxwrnlm0Po&usqp=CAU", "Bebida alcoholica");
         productoService.save(producto);
         producto = new Producto("Alfajor Oreo", "Oreo Milk", 51, 87, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQdD8r5tbcIp-_03h4xCMzMV-G9CTcJALyN1cpCO-8Ruru400dR&usqp=CAU", "Comida");
         productoService.save(producto);
         producto = new Producto("Harina 0000", "Pureza", 14, 52, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvrqMNHeCSbHTrtHf5xkZEDLw3JXEZUUVRDpr2huqRSrdBtPhY&usqp=CAU", "Comida");
+        productoService.save(producto);
+        producto = new Producto("Polenta Presto Pronta", "Presto Pronta", 20, 80, "https://images-na.ssl-images-amazon.com/images/I/31cbFb4iLSL.jpg", "Comida");
+        productoService.save(producto);
+        producto = new Producto("Dulce de leche", "Arcor", 30, 100, "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/image/1000x/040ec09b1e35df139433887a97daa66f/7/7/7790580504410.jpg", "Comida");
+        productoService.save(producto);
+        producto = new Producto("Bebida energetica", "Gatorade", 25, 83, "https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3050983_f.jpg", "Bebida sin alcohol");
+        productoService.save(producto);
+        producto = new Producto("Leche con almendras", "Silk", 17, 93, "https://d26lpennugtm8s.cloudfront.net/stores/209/218/products/whatsapp-image-2020-02-16-at-14-25-561-3e79fde0416fb9f67515818741544865-640-0.jpeg", "Bebida");
+        productoService.save(producto);
+        producto = new Producto("Cafe", "Arlistan", 33, 135, "https://walmartar.vteximg.com.br/arquivos/ids/835830-1000-1000/Cafe-Instantaneo-Arlistan-50-Gr-1-22269.jpg?v=636687343739270000", "Bebida");
         productoService.save(producto);
         /*producto = new Producto("Coca Cola en Lata", "Colca cola", 12, 65, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ9JZuIjzpIEe0LOajQmW5vQ5Nosn23CurhJQzYGhESF3GJMFUM&usqp=CAU", "Bebida");
         productoService.save(producto);*/
